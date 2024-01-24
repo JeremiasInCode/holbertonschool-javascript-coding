629a0ae17c5609d62df7ab169920f9fea7c4c7cc
Jeremias Erba <jeremiaserba7894@gmail.com>
2024-01-23 21:11:21 -0300

    Task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+request.get(apiUrl, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const movieData = JSON.parse(body);
+  console.log(movieData.title);
+});
