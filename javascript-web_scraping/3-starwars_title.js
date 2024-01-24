3ad54a9496942e7d75e1d041e03167af96123c4f
Jeremias Erba <jeremiaserba7894@gmail.com>
2024-01-23 21:13:19 -0300

    Task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+const api_url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
-request.get(apiUrl, (err, response, body) => {
+request.get(api_url, (err, response, body) => {
   if (err) {
     console.log(err);
   }
+
   const movieData = JSON.parse(body);
   console.log(movieData.title);
 });
